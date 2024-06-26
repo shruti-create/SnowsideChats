588c91a3f2ca154081fc9d3c1f23306e339483f4
shruti-create <shruti.bhamidipati@gmail.com>
2024-06-25 21:35:54 -0700

    connecting to mongodb for inseart/read functions

--- mongo_connect.py
+++ mongo_connect.py
@@ -3,13 +3,38 @@ from pymongo import MongoClient
 
 
 try:
+    # connecting
     uri = "mongodb+srv://JoshuaShruti:servicenow@clustersn0.aquderp.mongodb.net/"
     client = MongoClient(uri, server_api=pymongo.server_api.ServerApi(
         version="1", strict=True, deprecation_errors=True))
     client.admin.command("ping")
     print("Connected successfully")
+
+    # inserting data
+    database = client["user_data"]
+    collection = database["content"]
+    '''
+    collection.insert_many([
+            { "_id": 1, "user_name": "apples", "answers":"i like bananas"},
+            { "_id": 2, "user_name": "apples2", "answers":"i like bananas2"},
+            { "_id": 3, "user_name": "apples3", "answers":"i like bananas3"},       
+        ])
+    '''
+    
+    print("inserted successfully")
+    
+    # reading data
+    results = collection.find({})
+    for f in results: 
+        print(f)
+
     client.close()
 except Exception as e:
     raise Exception(
         "The following error occurred: ", e)
+
+
+
+
+
 
