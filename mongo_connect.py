7aa1e95961c7e6e07eb2b0d8b9df74442d9f52d7
shruti-create <shruti.bhamidipati@gmail.com>
2024-06-25 21:02:37 -0700

    new commit

---
+++ mongo_connect.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+
+try:
+    # start example code here
+    # end example code here
+    client.admin.command("ping")
+    print("Connected successfully")
+    # other application code
+    client.close()
+except Exception as e:
+    raise Exception(
+        "The following error occurred: ", e)
